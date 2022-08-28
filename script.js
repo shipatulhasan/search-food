7317344e2e16b7d290a45b9f29d827dc44a54815
shipatulhasan <shipatulhasan328@gmail.com>
2022-08-28 19:23:52 +0600

    adding webpage

---
+++ script.js
@@ -0,0 +1,50 @@
+const loadFood = async(search)=>{
+  try{
+
+    url=` https://www.themealdb.com/api/json/v1/1/search.php?s=${search} `
+    const res = await fetch(url)
+    const data = await res.json()
+    displayFood(data.meals)
+    
+  }
+  catch{
+    console.log(error)
+  }
+  
+
+}
+
+const displayFood = (foods)=>{
+
+  const container = document.getElementById('food-container')
+  container.textContent = ''
+
+  foods.forEach(food => {
+    const div = document.createElement('div')
+    div.classList.add('col')
+
+    div.innerHTML = `
+        <div class="card h-100">
+          <img src="${food.strMealThumb}" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">${food.strMeal}</h5>
+            <p class="card-text">${food.strInstructions.slice(0,120)}</p>
+          </div>
+        </div>
+      </div>
+    
+    `
+    container.appendChild(div)
+    
+  });
+
+}
+
+const search = (event)=>{
+  event.preventDefault()
+  const searchField= document.getElementById('search-field')
+  const searchItem = searchField.value
+  searchField.value = ''
+  loadFood(searchItem)
+  
+}
